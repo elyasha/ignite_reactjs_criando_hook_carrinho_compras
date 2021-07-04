330aadcf558790e6b195d33d01c7c2fad06b9b83
Matheus Elyasha <matheuselyasha@gmail.com>
2021-07-04 12:09:04 -0300

    resolved loadProducts with utils/format

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,7 +22,7 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
   // const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
@@ -31,7 +31,9 @@ const Home = (): JSX.Element => {
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      const _products = await api.get('/products').then((response) => response.data)
+      const formattedProducts = _products.map((product: Product) => {return {...product, priceFormatted: formatPrice(product.price)}})
+      setProducts(formattedProducts)
     }
 
     loadProducts();
